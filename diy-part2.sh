623d758ee1360502a38d4b4bb5d2b0b8b0c4a6e1
Papa Gaye <132003569+papagaye744@users.noreply.github.com>
2024-01-11 13:25:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,6 @@
 # ttyd 自动登录
 # sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
 
-# update golang version
-rm -rf feeds/packages/lang/golang
-svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
-
 # 添加自定义软件包
 echo '
 CONFIG_PACKAGE_luci-app-mosdns=y
